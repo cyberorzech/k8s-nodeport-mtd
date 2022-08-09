9a6e782af2bea52adb1e635a91af91d4e0fcb666
cyberorzech <natan.orze@gmail.com>
2022-08-09 14:54:17 +0100

    Implement check_cluster_connection function

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from kubernetes import client, config
 
-config.load_kube_config()
-# config.load_incluster_config()
+# config.load_kube_config()
+# # config.load_incluster_config()
 
-v1 = client.CoreV1Api()
-nodes = v1.list_node()
-print(nodes)
+# v1 = client.CoreV1Api()
+# nodes = v1.list_node()
+# print(nodes)
+
+def check_cluster_connection():
+    try:
+        config.load_kube_config()
+        v1 = client.CoreV1Api()
+        v1.list_node()
+        return True
+
+    except Exception as e:
+        if "Failed to establish a new connection" in str(e):
+            return False
+        else:
+            print(str(e))
+            return False
+
+def main():
+    check_cluster_connection()
+
+
+if __name__ == "__main__":
+    main()
