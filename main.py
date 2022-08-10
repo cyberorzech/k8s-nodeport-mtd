9e55aa52464553f750b3247b6ca76d0a021584e6
cyberorzech <natan.orze@gmail.com>
2022-08-10 17:08:04 +0100

    Create example patch yaml file

--- main.py
+++ main.py
@@ -3,16 +3,27 @@ from loguru import logger
 
 from mtd_sources.logger import initialize
 
-# config.load_kube_config()
-# # config.load_incluster_config()
+def setup():
+    # TODO
+    # stworz deployment x2
+    # sprawdz czy powstaly pody
+    # stworz nodeport x2
+    # sprawdz czy powstaly
+    # wez porty udostepnione
+    # sprawdz czy aplikacja jest dostepna
+    pass
 
-# v1 = client.CoreV1Api()
-# nodes = v1.list_node()
-# print(nodes)
+def cleanup(deployments, services):
+    # TODO
+    # sprawdz czy takie istnieja
+    # usun
+    pass
+
 
 def check_cluster_connection():
     try:
         config.load_kube_config()
+        # config.load_incluster_config()
         v1 = client.CoreV1Api()
         v1.list_node()
         return True
@@ -24,12 +35,13 @@ def check_cluster_connection():
             logger.error(f"{e}")
             return False
 
+
 def main():
     logger.success("jest ok")
     check_cluster_connection()
 
 
 if __name__ == "__main__":
-    CONFIG_FILE_PATH = "./config.yml"
+    CONFIG_FILE_PATH = "./config.yaml"
     initialize(CONFIG_FILE_PATH)
     main()
